34a63e3a55ff7ef1d2da96195ec561e768431844
Elvis Soares <elvis.asoares@gmail.com>
2023-06-19 15:09:42 -0300

    Adding ABC-FIRE optimization

--- pydftlj/dft3d.py
+++ pydftlj/dft3d.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy import pi, exp, log, sqrt, round, linspace, isscalar, array, meshgrid
 import timeit
+from tqdm import tqdm
 from .eos import LJEOS, BHdiameter
 from .fmtaux import sigmaLancsozFT,translationFT, w3FT, w2FT,phi1func,dphi1dnfunc,phi2func,dphi2dnfunc, phi3func, dphi3dnfunc, phi3funcWBI, dphi3dnfuncWBI, phi2funcWBII, dphi2dnfuncWBII, phi3funcWBII, dphi3dnfuncWBII
 from .dcf import  ljBH3dFT
@@ -39,7 +40,7 @@ class dft3d():
             if self.padding: 
                 if self.ljmethod == 'WDA':
                     self.Lgrid = Lcell + 4*self.psi*self.sigma
-                if self.ljmethod == 'MMFA':
+                if self.ljmethod == 'MFA' or self.ljmethod == 'MMFA':
                     self.Lgrid = Lcell + 8*self.sigma
                 else:
                     self.Lgrid = Lcell + self.sigma
@@ -116,7 +117,7 @@ class dft3d():
     def Set_Temperature(self,kT):
         self.kT = kT
         self.beta = 1/self.kT
-        if self.ljmethod == 'WDA' or self.ljmethod == 'MMFA':
+        if self.ljmethod == 'WDA' or self.ljmethod == 'MMFA' or self.ljmethod == 'MFA':
             self.d = round(BHdiameter(self.kT,sigma=self.sigma,epsilon=self.epsilon),3)
         else: self.d = self.sigma
 
@@ -136,6 +137,10 @@ class dft3d():
             self.mucore = lambda rr: self.mudisp(rr) - self.amft*rr
             self.uint = torch.zeros((self.Ngrid[0],self.Ngrid[1],self.Ngrid[2]),dtype=torch.float32)
             self.w_hat = torch.tensor(w3FT(self.K,sigma=2*self.d)*sigmaLancsozFT(self.Kx,self.Ky,self.Kz,self.kcut)/(4*pi*(self.d)**3/3),dtype=torch.complex64, device=device)
+            self.ulj_hat = torch.tensor(ljBH3dFT(self.K,self.kT,self.sigma,self.epsilon)*sigmaLancsozFT(self.Kx,self.Ky,self.Kz,self.kcut),dtype=torch.complex64, device=device) # to avoid Gibbs phenomenum
+        elif self.ljmethod == 'MFA':
+            self.amft = -32*pi*self.epsilon*self.sigma**3/9
+            self.uint = torch.zeros((self.Ngrid[0],self.Ngrid[1],self.Ngrid[2]),dtype=torch.float32)
             self.ulj_hat = torch.tensor(ljBH3dFT(self.K,self.kT,self.sigma,self.epsilon)*sigmaLancsozFT(self.Kx,self.Ky,self.Kz,self.kcut),dtype=torch.complex64, device=device) # to avoid Gibbs phenomenum
 
     def Set_BulkDensity(self,rhob):
@@ -184,7 +189,6 @@ class dft3d():
                     self.Vext[:,:,-self.Npad[2]:] = 0.
 
     def Padding_Density(self):
-
         if self.padding: 
             if self.Ncell[0] == 0 and self.Ncell[1] == 0:
                 self.rho[:,:,:self.Npad[2]] = self.pad_constant
@@ -202,8 +206,8 @@ class dft3d():
     def Set_InitialCondition(self):
 
         self.rho[:] = 0.0
-        self.rho[self.mask] = self.rhob
-        # self.rho[self.mask] = self.rhob*np.exp(-0.01*self.beta*self.Vext[self.mask].cpu())
+        # self.rho[self.mask] = self.rhob
+        self.rho[self.mask] = self.rhob*np.exp(-0.01*self.beta*self.Vext[self.mask].cpu())
 
         if self.padding: 
             self.Padding_Density()
@@ -253,9 +257,9 @@ class dft3d():
         self.n2vec[1] = torch.fft.ifftn(self.rho_hat*self.w2vec_hat[1]).real.cpu()
         self.n2vec[2] = torch.fft.ifftn(self.rho_hat*self.w2vec_hat[2]).real.cpu()
 
-        self.n3[self.n3>=1.0] = 1.0-1e-9 # to avoid Nan on some calculations
-        self.xi = (self.n2vec*self.n2vec).sum(dim=0)/((self.n2+1e-16)**2)
-        self.xi[self.xi>=1.0] = 1.0-1e-9
+        self.n3[self.n3>=1.0] = 1.0-1e-30 # to avoid Nan on some calculations
+        self.xi = (self.n2vec*self.n2vec).sum(dim=0)/((self.n2+1e-30)**2)
+        self.xi[self.xi>=1.0] = 1.0-1e-30
 
         self.n1vec[:] = self.n2vec/(2*pi*self.d)
 
@@ -287,9 +291,11 @@ class dft3d():
             self.rhobar[:] = torch.fft.ifftn(self.rho_hat*self.w_hat).real.cpu()
             self.uint[:] = torch.fft.ifftn(self.rho_hat*self.ulj_hat).real.cpu()
             self.mu_hat[:] =  torch.fft.fftn(torch.tensor(self.mucore(self.rhobar.numpy()),dtype=torch.float32, device=device))
+        elif self.ljmethod == 'MFA':
+            self.uint[:] = torch.fft.ifftn(self.rho_hat*self.ulj_hat).real.cpu()
 
     def Calculate_Free_energy(self):
-        self.Fid = self.kT*torch.sum(self.rho*(torch.log(self.rho+1.0e-16)-1.0))*self.dV
+        self.Fid = self.kT*torch.sum(self.rho*(torch.log(self.rho+1.0e-30)-1.0))*self.dV
 
         phi = self.n0*self.phi1+self.phi2*(self.n1*self.n2-(self.n1vec*self.n2vec).sum(dim=0)) + self.phi3*self.n2**3*(1-self.xi)**3
         
@@ -300,6 +306,9 @@ class dft3d():
             self.Flj = torch.sum(phi)*self.dV
         elif self.ljmethod == 'MMFA':
             phi[:] = 0.5*self.rho*self.uint + torch.tensor(self.fcore(self.rhobar.numpy()),dtype=torch.float32)
+            self.Flj = torch.sum(phi)*self.dV
+        elif self.ljmethod == 'MFA':
+            phi[:] = 0.5*self.rho*self.uint
             self.Flj = torch.sum(phi)*self.dV
         else:
             self.Flj = 0.0
@@ -337,6 +346,8 @@ class dft3d():
             self.c1att[:] = -self.beta*torch.fft.ifftn(self.mu_hat*self.w_hat).real
         elif self.ljmethod == 'MMFA':
             self.c1att[:] = -self.beta*self.uint.to(device) -self.beta*torch.fft.ifftn(self.mu_hat*self.w_hat).real
+        elif self.ljmethod == 'MFA':
+            self.c1att[:] = -self.beta*self.uint.to(device)
 
         self.c1[:] = self.c1hs + self.c1att
 
@@ -374,6 +385,8 @@ class dft3d():
 
         if self.ljmethod == 'WDA' or self.ljmethod == 'MMFA':
             self.muatt = self.mudisp(self.rhob)
+        elif self.ljmethod == 'MFA':
+            self.muatt = self.rhob*self.amft
         else:
             self.muatt = 0.0
 
@@ -388,7 +401,7 @@ class dft3d():
         self.dt = torch.tensor(dt, device=device)
 
         lnrho = torch.zeros((self.Ngrid[0],self.Ngrid[1],self.Ngrid[2]),dtype=torch.float32, device=device)
-        lnrho[:] = torch.log(self.rho+1.0e-16) # to avoid log(0)
+        lnrho[:] = torch.log(self.rho+1.0e-30) # to avoid log(0)
         self.Update_System()
 
         F = torch.zeros((self.Ngrid[0],self.Ngrid[1],self.Ngrid[2]),dtype=torch.float32, device=device)
@@ -463,9 +476,105 @@ class dft3d():
                 sk[:]=atol+rtol*torch.abs(self.rho)
                 error = torch.norm(self.rho.to(device)*F/sk)/np.sqrt(self.Ngridtot)
                 if logoutput: print(self.Niter,self.Omega.numpy(),error.cpu().numpy())
-                if error < 1.0: break
+                if error < 1.0 and self.Niter> Ndelay: break
                 
             del V
+
+        elif method == 'abc-fire':
+            # ABC-Fire algorithm https://doi.org/10.1016/j.commatsci.2022.111978
+            Ndelay = 20
+            Nnegmax = 2000
+            dtmax = 10*dt
+            dtmin = 0.02*dt
+            Npos = 1
+            Nneg = 0
+            finc = 1.1
+            fdec = 0.5
+            fa = 0.99
+            V = torch.zeros((self.Ngrid[0],self.Ngrid[1],self.Ngrid[2]),dtype=torch.float32, device=device)
+
+            for i in range(max_iter):
+
+                P = torch.sum(F*V) # dissipated power
+                if (P>0):
+                    Npos = Npos + 1
+                    if Npos>Ndelay:
+                        dt = min(dt*finc,dtmax)
+                        alpha = max(1e-10,alpha*fa)
+                else:
+                    Npos = 1
+                    Nneg = Nneg + 1
+                    if Nneg > Nnegmax: break
+                    if i> Ndelay:
+                        dt = max(dt*fdec,dtmin)
+                        alpha = alpha0
+                    lnrho[:] -= V*0.5*dt
+                    V[:] = 0.0
+                    self.rho[:] = torch.exp(lnrho).cpu()
+                    if self.padding: 
+                        self.Padding_Density()
+                    self.Update_System()
+
+                V[:] += F*0.5*dt
+                V[:] = (1/(1-(1-alpha)**Npos))*((1-alpha)*V + alpha*F*torch.norm(V)/torch.norm(F))
+                lnrho[:] += dt*V
+                self.rho[:] = torch.exp(lnrho).cpu()
+                if self.padding: 
+                    self.Padding_Density()
+                self.Update_System()
+                F[:] = -(lnrho - self.c1 - self.beta*self.mu + self.beta*self.Vext)
+                V[:] += F*0.5*dt
+
+                self.Niter = i+1
+                sk[:]=atol+rtol*torch.abs(self.rho)
+                error = torch.norm(self.rho.to(device)*F/sk)/np.sqrt(self.Ngridtot)
+                if logoutput: print(self.Niter,self.Omega.numpy(),error.cpu().numpy())
+                if error < 1.0 and self.Niter> Ndelay: break
+                
+            del V
+
+        elif method == 'metropolis':
+
+            self.sigma_lnrho = 0.001
+            burning_steps = int(max_iter/10)
+            self.rho_ensemble = torch.zeros((int(max_iter),self.Ngrid[0],self.Ngrid[1],self.Ngrid[2]),dtype=torch.float32, device=device)
+            self.Omega_ensemble = torch.zeros(int(max_iter),dtype=torch.float32, device=device)
+            lnrho_prev = torch.tensor(lnrho,dtype=torch.float32, device=device)
+            Omega_prev = self.Omega
+            self.lnrho_hi = 4.0
+            self.lnrho_lo = -70.0
+
+            for i in tqdm(range(int(max_iter))):
+
+                lnrho_prop = torch.normal(mean=lnrho_prev,std=self.sigma_lnrho).to(device)
+                # reflect proposals outside of bounds
+                too_hi = lnrho_prop > self.lnrho_hi
+                too_lo = lnrho_prop < self.lnrho_lo
+
+                lnrho_prop[too_hi] = 2*self.lnrho_hi - lnrho_prop[too_hi]
+                lnrho_prop[too_lo] = 2*self.lnrho_lo - lnrho_prop[too_lo]
+
+                self.rho[:] = torch.exp(lnrho_prop).cpu()
+                if self.padding: 
+                    self.Padding_Density()
+                self.Update_System()
+
+                Omega_prop = self.Omega
+
+                U = torch.rand(1,device=device)
+                ratio = torch.minimum(torch.tensor([1.0],device=device), torch.exp(-self.beta*(Omega_prop-Omega_prev)))
+                # print(U,ratio)
+                if (U <= ratio):
+                    self.rho_ensemble[i,:] = self.rho
+                    self.Omega_ensemble[i] = Omega_prop
+                    lnrho_prev = lnrho_prop
+                    Omega_prev = Omega_prop
+                else:
+                    self.rho_ensemble[i,:] = torch.exp(lnrho_prev)
+                    self.Omega_ensemble[i] = Omega_prev
+
+            self.rho = self.rho_ensemble[burning_steps:,:].mean(axis=0).cpu()
+            self.Update_System()
 
         elif method == 'rkf2(3)':
             # Fehlberg method 1(2)
--- pydftlj/fmtaux.py
+++ pydftlj/fmtaux.py
@@ -56,41 +56,41 @@ def dphi2dnfuncWBII(eta):
 @vectorize
 def phi3func(eta):
     if eta<=1e-3:
-        return 24*pi*(1+2*eta+3*eta**2)
+        return (1+2*eta+3*eta**2)/(24*pi)
     else: 
         return 1/(24*pi*(1-eta)**2)
     
 @vectorize
 def dphi3dnfunc(eta):
     if eta<=1e-3:
-        return 24*pi*(2+6*eta+12*eta**2)
+        return (2+6*eta+12*eta**2)/(24*pi)
     else: 
         return 1/(12*pi*(1-eta)**3)
      
 @vectorize
 def phi3funcWBI(eta):
     if eta<=1e-3:
-        return 36*pi*(1.5+8*eta/3+15*eta**2/4)
+        return (1.5+8*eta/3+15*eta**2/4)/(36*pi)
     else: 
         return (eta+(1-eta)**2*log(1-eta))/(36*pi*eta**2*(1-eta)**2)
     
 @vectorize
 def dphi3dnfuncWBI(eta):
     if eta<=1e-3:
-        return 36*pi*(8/3+7.5*eta+72*eta**2/5)
+        return (8/3+7.5*eta+72*eta**2/5)/(36*pi)
     else: 
         return -(eta*(2-5*eta+eta**2)+2*(1-eta)**3*log(1-eta))/(36*pi*eta**3*(1-eta)**3)
 
 @vectorize
 def phi3funcWBII(eta):
     if eta<=1e-3:
-        return 36*pi*(1.5+7*eta/3+13*eta**2/4)
+        return (1.5+7*eta/3+13*eta**2/4)/(36*pi)
     else: 
         return -(eta+(eta-3)*eta**2+log(1-eta)*(1-eta)**2)/(36*pi*eta**2*(1-eta)**2)
     
 @vectorize
 def dphi3dnfuncWBII(eta):
     if eta<=1e-3:
-        return 36*pi*(7/3+6.5*eta+63*eta**2/5)
+        return (7/3+6.5*eta+63*eta**2/5)/(36*pi)
     else: 
         return -(eta*(-2+5*eta-6*eta**2+eta**3)-2*(1-eta)**3*log(1-eta))/(36*pi*eta**3*(1-eta)**3)
